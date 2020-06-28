0732fb1df7b4b2c8bf8ea7d931889d9e2048d633
David Bosak <dbosak01@gmail.com>
2020-06-27 21:03:54 -0400

    Changing name

---
+++ R/reportr.R
@@ -0,0 +1,11 @@
+#' @title reportr: A package for creating tabular reports
+#'
+#' @description The reportr package creates tabular reports in ASCII text and
+#' docx format
+#'
+#' @section reportr functions:
+#' hello()
+#'
+#' @docType package
+#' @name reportr
+NULL
